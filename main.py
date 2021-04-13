ab7d6e2dc580abd005d768af13377d5cad8d0acb
hopfn <nils.hopf@beuth-hochschule.de>
2021-04-13 11:36:14 +0200

    Abändern der Main; Hinzufügen der taster.py mit LED Test Funktion

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import functions
-def print_hi(name):
-    print(f'Hello, {name}')
+import taster
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    fruits = ["banana", "apple", "pineapple"]
-    for i in fruits:
-        #print_hi('PyCharm')
-        if i == "banana":
-            n = 0
-            while n != 5:
-                functions.print_hi(i)
-                print_hi(i)
-                functions.secondfunction()
-                n += 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        taster.blinkt()
